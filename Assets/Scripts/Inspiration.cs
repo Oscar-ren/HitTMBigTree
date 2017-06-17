776a1bcc89fa5422aadbc8d569be0099abd9e454
明非 <az8641683@163.com>
2017-06-18 00:44:22 +0800

    添加灵感

---
+++ Assets/Scripts/Inspiration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inspiration : MonoBehaviour {
+    public int inspiration;
+
+    public int Get () {
+        return inspiration;
+    }
+    public void Add (int num) {
+        inspiration += num;
+    }
+    public void Use(int num)
+    {
+        inspiration -= num;
+        if (inspiration < 0)
+            inspiration = 0;
+    }
+    public bool Check () {
+        return inspiration > 0;
+    }
+
+}
---
+++ Assets/Scripts/OpenBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour {
+    public Inspiration Ins;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box") {
+            Destroy(other.gameObject);
+            Ins.Add(Random.Range(5,16));
+        }
+    }
+}
