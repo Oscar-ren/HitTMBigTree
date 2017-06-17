34cd96722290555615649bd10e1d831d6f7b8afb
renxianglei <renxianglei@renxianleideMBP.lan>
2017-06-18 02:34:09 +0800

    ...

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager {
+
+	public static bool isOver = false;
+
+	public static void GameOver()
+	{
+		if(isOver == false)
+			SceneManager.LoadScene ("GameOver", LoadSceneMode.Additive);
+		isOver = true;
+	}
+}
