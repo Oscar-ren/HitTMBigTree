3256f960c9ee5a7804813444937e726be9b4eb6a
明非 <az8641683@163.com>
2017-06-18 04:28:50 +0800

    结束动画

---
+++ Assets/Scripts/CameraAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnim : MonoBehaviour {
+	public GameObject p1;
+	public GameObject p2;
+	public GameObject p3;
+	public GameObject p4;
+	// Use this for initialization
+	void Start () {
+        //Fly();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Set (GameObject p, Vector3 pos, float x) {
+        p.transform.position = pos;
+        p.GetComponent<Animator>().SetTrigger("Victory");
+        p.transform.rotation = Quaternion.Euler(new Vector3(0, x, 0));
+    }
+
+    public void Fly() {
+        GetComponent<CameraManager>().enabled = false;
+        GetComponent<Animator>().SetTrigger("Fly");
+		Set(p1, new Vector3(-20f, -8.72f, 55.9f), 40);
+		Set(p2, new Vector3(-19.9f, -8.34f, 57.8f), 56);
+		Set(p3, new Vector3(-25f, -8.67f, 54.41f), 5);
+		Set(p4, new Vector3(-23f, -8.15f, 56.61f), -18.85f);
+    }
+}
