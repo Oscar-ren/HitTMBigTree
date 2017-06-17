c4415862c5b7b3b2592f21ff7405d96700fdd9fb
renxianglei <renxianglei@renxianleideMBP.lan>
2017-06-17 18:15:30 +0800

    fix

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,20 +5,35 @@ using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour {
 
-	Transform player;
+	GameObject[] players;
 	UnityEngine.AI.NavMeshAgent nav;
 
 
 	void Awake ()
 	{
-		player = GameObject.FindGameObjectsWithTag ("Player")[Random.Range(0, 2)].transform;
+//		player = GameObject.FindGameObjectsWithTag ("Player")[Random.Range(0, 2)].transform;
 		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
 	}
 
 
 	void Update ()
 	{
+
+		players = GameObject.FindGameObjectsWithTag ("Player");
+		GameObject player = players[0];
+		float minValue = 1000000f;;
+
+		for(int i = 0; i < players.Length; i++)
+		{
+			Vector3 diff = players [i].transform.position - transform.position;
+			if(diff.sqrMagnitude < minValue)
+			{
+				minValue = diff.sqrMagnitude;
+				player = players [i];
+			}
+		}
+
 		GetComponent<Animator>().SetFloat("Speed", 1.0f);
-		nav.SetDestination (player.position);
+		nav.SetDestination (player.transform.position);
 	}
 }
