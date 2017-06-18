1685a1072bfba4c6b7648358687b58cfed7e3705
明非 <az8641683@163.com>
2017-06-18 14:10:06 +0800

    x

---
+++ Assets/Scripts/BossMusic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMusic : MonoBehaviour {
+
+    private bool isMusic;
+    public AudioClip ac;
+    public AudioSource sm;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !isMusic) {
+            isMusic = true;
+            sm.clip = ac;
+            sm.Play();
+        }
+    }
+
+}
